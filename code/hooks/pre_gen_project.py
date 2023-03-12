1a88bf5755781015e2cb0c76134832b86b74a2d0
David Cuy <david.cuy.sanchez@gmail.com>
2023-03-12 03:29:50 -0600

    fix: add reeplace in string

--- code/hooks/pre_gen_project.py
+++ code/hooks/pre_gen_project.py
@@ -20,13 +20,14 @@ elif db_dialect == "mssql":
     db_port = "1433"
 
 db_conn = f"{db_driver}:///app.db" if db_dialect == "sqlite" else f"{db_dialect}+{db_driver}://{db_user}:{db_pass}@{db_host}/{db_name}"
-cookiecutter_update = f"""{{
+cookiecutter_update = f"""##start##start cookiecutter.update(##start
     "_dbDriver": "{db_driver}",
     "_db_port": "{db_port}",
     "_dbConn": "{db_conn}"
-}}"""
+##end) ##end##end""".replace("##start", "{").replace("##end", "}")
 
+print(cookiecutter_update)
 
-f"""{{{{ cookiecutter.update({cookiecutter_update})}}}}"""
+f"""{cookiecutter_update}"""
 
 sys.exit(0)
