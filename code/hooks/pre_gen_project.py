e1e6a9d68b8fbfc98d69ca0becef534658936cfb
David Cuy <david.cuy.sanchez@gmail.com>
2023-03-12 03:14:48 -0600

    fix: string compare

--- code/hooks/pre_gen_project.py
+++ code/hooks/pre_gen_project.py
@@ -19,7 +19,7 @@ elif db_dialect == "mssql":
     db_driver = "pyodbc"
     db_port = "1433"
 
-db_conn = f"{db_driver}:///app.db" if db_dialect is "sqlite" else f"{db_dialect}+{db_driver}://{db_user}:{db_pass}@{db_host}/{db_name}"
+db_conn = f"{db_driver}:///app.db" if db_dialect == "sqlite" else f"{db_dialect}+{db_driver}://{db_user}:{db_pass}@{db_host}/{db_name}"
 print(db_conn)
 
 """{{ cookiecutter.update(
