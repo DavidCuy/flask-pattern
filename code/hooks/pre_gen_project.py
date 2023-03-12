2a3ed9198541be817189f308c189c64545d4056a
David Cuy <david.cuy.sanchez@gmail.com>
2023-03-12 03:34:31 -0600

    fix: try delete first braces

--- code/hooks/pre_gen_project.py
+++ code/hooks/pre_gen_project.py
@@ -20,11 +20,11 @@ elif db_dialect == "mssql":
     db_port = "1433"
 
 db_conn = f"{db_driver}:///app.db" if db_dialect == "sqlite" else f"{db_dialect}+{db_driver}://{db_user}:{db_pass}@{db_host}/{db_name}"
-cookiecutter_update = f"""##start##start cookiecutter.update(##start
+cookiecutter_update = f"""cookiecutter.update(##start
     "_dbDriver": "{db_driver}",
     "_db_port": "{db_port}",
     "_dbConn": "{db_conn}"
-##end) ##end##end""".replace("##start", "{").replace("##end", "}")
+##end)""".replace("##start", "{").replace("##end", "}")
 
 
 f"""{{ {cookiecutter_update} }}"""
