20f6414576297e9c47c13fd5946911a79afc897b
David Cuy <david.cuy.sanchez@gmail.com>
2023-03-12 03:05:50 -0600

    fix: conn variable

--- code/hooks/pre_gen_project.py
+++ code/hooks/pre_gen_project.py
@@ -19,11 +19,13 @@ elif db_dialect == "mssql":
     db_driver = "pyodbc"
     db_port = "1433"
 
+db_conn = f"{db_driver}:///app.db" if db_dialect is "sqlite" else f"{db_dialect}+{db_driver}://{db_user}:{db_pass}@{db_host}/{db_name}"
+
 
 f"""{{{{ cookiecutter.update({{
         "_dbDriver": {db_driver},
         "_db_port": {db_port},
-        "_dbConn": f"{db_driver}:///app.db" if db_dialect is "sqlite" else f"{db_dialect}+{db_driver}://{db_user}:{db_pass}@{db_host}/{db_name}"
+        "_dbConn": {db_conn}
     }})
 }}}}"""
 
