9579b72e1d440d5487c415fec9ba5d4979d4e6f2
David Cuy <david.cuy.sanchez@gmail.com>
2023-03-28 23:14:31 -0600

    feature: add .env generation

--- code/hooks/post_gen_project.py
+++ code/hooks/post_gen_project.py
@@ -1,8 +1,10 @@
 import os
 import sys
 
+PROJECT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '{{ cookiecutter.repository }}')
+
 REMOVE_PATHS = [
-    '{% if cookiecutter.dbDialect != "mssql" %} odbcinst.ini {% endif %}',
+    {% if cookiecutter.dbDialect != "mssql" %} os.path.join(PROJECT_DIR, 'odbcinst.ini'), {% endif %}
 ]
 
 for path in REMOVE_PATHS:
@@ -12,3 +14,23 @@ for path in REMOVE_PATHS:
             os.rmdir(path)
         else:
             os.unlink(path)
+
+env_body = """APP_NAME="<APP NAME>"
+APP_URL="<APP URL>"
+APP_DESCRIPTION="<APP DESCRIPTION>"
+ENVIRONMENT="<ENVIRONMENT>"
+
+DB_USER="<DB USER>"
+DB_PASSWORD="<DB PASSWORD>"
+DB_HOST="<DB HOST>"
+DB_SCHEMA="<DB SCHEMA>"
+DB_DRIVER="<DB DRIVER>"
+DB_PORT="<DB PORT>"
+DB_CONNECTION_STRING="<DB CONNECTION STRING>"
+"""
+
+os.rename(os.path.join(PROJECT_DIR, '.env.dist'), os.path.join(PROJECT_DIR, '.env'))
+
+with open(os.path.join(PROJECT_DIR, '.env.dist'), 'w+') as f:
+    f.write(env_body)
+
