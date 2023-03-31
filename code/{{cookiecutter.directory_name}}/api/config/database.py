119ce5a1d8849ff7be44b7bdb0e8f7da81fb9e89
David Cuy <david.cuy.sanchez@gmail.com>
2023-03-31 00:51:23 -0600

    feature; add docker-compsoe support

--- code/{{cookiecutter.directory_name}}/api/config/database.py
+++ code/{{cookiecutter.directory_name}}/api/config/database.py
@@ -1,7 +1,7 @@
 import Environment as env
 
 config = {
-    'sqlite': {
+    '{{cookiecutter.dbDialect == "mysql" ? "pymysql" : cookiecutter.dbDialect == "postgresql" ? "psycopg2" : cookiecutter.dbDialect == "mssql" ? "pyodbc" : "sqlite" }}': {
         'conn_string': env.DB_CONNECTION_STRING
     }
 }
