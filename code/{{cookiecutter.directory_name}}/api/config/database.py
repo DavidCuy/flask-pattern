e9b9263267ca1948287281525817b8a14a533a8f
David Cuy <david.cuy.sanchez@gmail.com>
2023-03-31 00:56:18 -0600

    fix: shorthand if else

--- code/{{cookiecutter.directory_name}}/api/config/database.py
+++ code/{{cookiecutter.directory_name}}/api/config/database.py
@@ -1,7 +1,7 @@
 import Environment as env
 
 config = {
-    '{{cookiecutter.dbDialect == "mysql" ? "pymysql" : cookiecutter.dbDialect == "postgresql" ? "psycopg2" : cookiecutter.dbDialect == "mssql" ? "pyodbc" : "sqlite" }}': {
+    '{{ "pymysql" if cookiecutter.dbDialect == "mysql" else "psycopg2" if cookiecutter.dbDialect == "postgresql" else "mssql" cookiecutter.dbDialect == "mssql" else "sqlite" }}': {
         'conn_string': env.DB_CONNECTION_STRING
     }
 }
