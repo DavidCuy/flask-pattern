f84073e59cc76390fd7f2b0ff97cb7143be901a0
David Cuy <david.cuy.sanchez@gmail.com>
2023-03-31 00:57:35 -0600

    fix: missing if

--- code/{{cookiecutter.directory_name}}/api/config/database.py
+++ code/{{cookiecutter.directory_name}}/api/config/database.py
@@ -1,7 +1,7 @@
 import Environment as env
 
 config = {
-    '{{ "pymysql" if cookiecutter.dbDialect == "mysql" else "psycopg2" if cookiecutter.dbDialect == "postgresql" else "mssql" cookiecutter.dbDialect == "mssql" else "sqlite" }}': {
+    '{{ "pymysql" if cookiecutter.dbDialect == "mysql" else "psycopg2" if cookiecutter.dbDialect == "postgresql" else "mssql" if cookiecutter.dbDialect == "mssql" else "sqlite" }}': {
         'conn_string': env.DB_CONNECTION_STRING
     }
 }
