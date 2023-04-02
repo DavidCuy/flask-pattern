f7c03a02c204909a5a94d759195cfbe8eb8d8d12
David Cuy <david.cuy.sanchez@gmail.com>
2023-04-01 21:17:22 -0600

    feature: refactor env and db variables

--- code/{{cookiecutter.directory_name}}/api/config/database.py
+++ code/{{cookiecutter.directory_name}}/api/config/database.py
@@ -1,7 +1,7 @@
 import Environment as env
 
 config = {
-    '{{ "pymysql" if cookiecutter.dbDialect == "mysql" else "psycopg2" if cookiecutter.dbDialect == "postgresql" else "mssql" if cookiecutter.dbDialect == "mssql" else "sqlite" }}': {
+    '{{cookiecutter._driver}}': {
         'conn_string': env.DB_CONNECTION_STRING
     }
 }
